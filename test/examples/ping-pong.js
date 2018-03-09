2082acbdc9317e1efd3a174c052c57b94dca9be0
pnhung177 <pnhung177@acegik.net>
2018-03-09 10:12:33 +0700

    Add the ping-pong example

---
+++ test/examples/ping-pong.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var Loadsync = require('loadsync');
+var lodash = require('devebot').require('lodash');
+
+var socket = require('socket.io-client')('http://127.0.0.1:7878', {
+  'force new connection': true,
+  reconnect: true
+});
+
+var demoData = {
+  'action_1': {
+    number: 1024
+  },
+  'action_2': {
+    pi: 3.14159
+  },
+  'action_3': {
+    connectionOption: {
+      host: '0.0.0.0',
+      port: 8080
+    }
+  }
+}
+
+var loadsync = new Loadsync([{
+  name: 'NO_TIMEOUT',
+  cards: lodash.keys(demoData)
+}]);
+
+loadsync.ready(function(info) {
+  socket.close();
+}, 'NO_TIMEOUT');
+
+socket.on('connect', function() {
+  lodash.forOwn(demoData, function(data, action) {
+    socket.on(action, function(info) {
+      console.log('Result of %s: %s', action, JSON.stringify(info, null, 2));
+      loadsync.check(action, 'NO_TIMEOUT');
+    });
+    socket.emit(action, data);
+  });
+});
